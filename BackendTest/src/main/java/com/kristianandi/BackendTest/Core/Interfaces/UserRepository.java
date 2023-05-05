1514e8a9626a67b6c2528bb12a62a49154d8b5ba
Kristian Andi <kristianandi33@gmail.com>
2023-05-05 13:37:29 +0700

    Add query to User repository

--- BackendTest/src/main/java/com/kristianandi/BackendTest/Core/Interfaces/UserRepository.java
+++ BackendTest/src/main/java/com/kristianandi/BackendTest/Core/Interfaces/UserRepository.java
@@ -2,7 +2,14 @@ package com.kristianandi.BackendTest.Core.Interfaces;
 
 import com.kristianandi.BackendTest.Core.Domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    @Query("select u from User u where u.phoneNumber = ?1 and u.password = ?2")
+    List<User> findByPhoneNumberAndPassword(@Param("phone_number") String phoneNumber,
+                                                           @Param("password") String password);
 }
