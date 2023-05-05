fa59fdb5d5aa8f0654ae1b8a0c6933136b55b7a9
Kristian Andi <kristianandi33@gmail.com>
2023-05-05 13:37:56 +0700

    Add basic create user endpoint

--- BackendTest/src/main/java/com/kristianandi/BackendTest/Rest/UserController.java
+++ BackendTest/src/main/java/com/kristianandi/BackendTest/Rest/UserController.java
@@ -3,12 +3,19 @@ package com.kristianandi.BackendTest.Rest;
 import com.kristianandi.BackendTest.Core.Domain.ApiResponse;
 import com.kristianandi.BackendTest.Core.Domain.User;
 import com.kristianandi.BackendTest.Core.Interfaces.UserRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class UserController {
@@ -16,7 +23,7 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/user")
+    @GetMapping("/users")
     private List<User> GetAllUser()
     {
         List<User> listUsers = userRepository.findAll();
@@ -24,14 +31,28 @@ public class UserController {
         return listUsers;
     }
 
-    @PostMapping(value = "/user", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    private ApiResponse CreateUser(@RequestBody User newUser)
+    @PostMapping(value = "/users", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    private ApiResponse CreateUser(@Valid @RequestBody User newUser)
     {
         User user = userRepository.save(newUser);
         ApiResponse response = new ApiResponse(201, "Created");
         response.data = user;
 
         return  response;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return errors;
     }
 
 }
